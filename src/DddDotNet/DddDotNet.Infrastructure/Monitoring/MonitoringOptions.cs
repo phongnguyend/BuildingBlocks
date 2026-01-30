82f70ee12ea01ea84d86469946222df1529e3a95
Phong Nguyen <nguyendoanphong.it@gmail.com>
2026-01-30 07:51:00 +0700

    (#32) Deprecating Zipkin Exporter

--- src/DddDotNet/DddDotNet.Infrastructure/Monitoring/MonitoringOptions.cs
+++ src/DddDotNet/DddDotNet.Infrastructure/Monitoring/MonitoringOptions.cs
@@ -1,13 +1,10 @@
 ﻿using DddDotNet.Infrastructure.Monitoring.AzureApplicationInsights;
-using DddDotNet.Infrastructure.Monitoring.MiniProfiler;
 using DddDotNet.Infrastructure.Monitoring.OpenTelemetry;
 
 namespace DddDotNet.Infrastructure.Monitoring;
 
 public class MonitoringOptions
 {
-    public MiniProfilerOptions MiniProfiler { get; set; }
-
     public AzureApplicationInsightsOptions AzureApplicationInsights { get; set; }
 
     public OpenTelemetryOptions OpenTelemetry { get; set; }
